5e34e98cfba590ff6d67a1c2a1730fa3e20e3db2
ajitsinghkaler <ajitsinghkaler0@gmail.com>
2020-10-17 13:57:34 +0530

    Small Chnages

--- index.ts
+++ index.ts
@@ -3,23 +3,21 @@ import "./style.css";
 
 // RxJS v6+
 import { fromEvent } from "rxjs";
-import { throttleTime, tap } from "rxjs/operators";
+import { throttleTime, tap, debounceTime } from "rxjs/operators";
 
 const indicator = document.getElementById("indication");
 
-const body = document.body,
-  html = document.documentElement;
-console.log(indicator);
+const html = document.documentElement;
 
 const getScrollPercentage = () => {
-  const winScroll = body.scrollTop || html.scrollTop;
+  const winScroll = html.scrollTop;
   const height = html.scrollHeight - html.clientHeight;
   return (winScroll / height) * 100;
 };
 
-const setScrollWidth = _ =>
-  (indicator.style.width = getScrollPercentage() + "%");
-
 fromEvent(document, "scroll")
-  .pipe(throttleTime(20))
-  .subscribe(setScrollWidth);
+  .pipe(
+    throttleTime(20),
+    tap(_ => (indicator.style.width = getScrollPercentage() + "%"))
+  )
+  .subscribe();
